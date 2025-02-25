8a5e6a1da9ec44c86d043c21e91ce37bee34e57e
1MisterE <rocket.god245@inbox.ru>
2025-02-25 18:42:28 +0300

    Update mars.py task:Рекламная кампания

--- mars.py
+++ mars.py
@@ -2,15 +2,28 @@ from flask import Flask, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
-def qls():
-	return f"Миссия Колонизация Марса"
 
-@app.route('/index')
+@app.route("/")
+def home():
+    return f"Миссия Колонизация Марса"
+
+
+@app.route("/index")
 def index():
     return "И на Марсе будут яблони цвести!"
 
 
+@app.route("/promote")
+def promote():
+    s = [
+        "Человечество вырастает из детства.",
+        "Человечеству мала одна планета.",
+        "Мы сделаем обитаемыми безжизненные пока планеты.",
+        "И начнем с Марса!",
+        "Присоединяйся!",
+    ]
+    return "</br>".join(s)
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5252)
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5252)
