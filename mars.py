71449ab832d4e065a83cac5c057a7086ce512394
1MisterE <rocket.god245@inbox.ru>
2025-03-14 14:03:50 +0300

    Update mars.py

--- mars.py
+++ mars.py
@@ -23,6 +23,21 @@ def promote():
         "Присоединяйся!",
     ]
     return "</br>".join(s)
+@app.route("/image_mars")
+def image_draw():
+	return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <title>Привет, Марс!</title>
+                  </head>
+                  <body>
+                    <h1>Жди нас, Марс!</h1>
+                    <img src="{url_for('static', filename='img/image_mars.jpg')}"
+           alt="здесь должна была быть картинка, но не нашлась">
+                    <h2>Вот она какая, красная конфета</h2>
+                  </body>
+                </html>"""
 
 
 if __name__ == "__main__":
